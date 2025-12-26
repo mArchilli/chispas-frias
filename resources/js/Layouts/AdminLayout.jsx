5613838924a152eec57aa51abef2828f22bf1fbf
mArchilli <archillimatias@gmail.com>
2025-12-26 13:46:16 -0300

    cambios en navbar de admin

--- resources/js/Layouts/AdminLayout.jsx
+++ resources/js/Layouts/AdminLayout.jsx
@@ -51,26 +51,6 @@ export default function AdminLayout({ children, header = null }) {
             ),
             current: false 
         },
-        { 
-            name: 'Órdenes', 
-            href: '/admin/orders', 
-            icon: (
-                <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-                </svg>
-            ),
-            current: false 
-        },
-        { 
-            name: 'Usuarios', 
-            href: '/admin/users', 
-            icon: (
-                <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z" />
-                </svg>
-            ),
-            current: false 
-        },
     ];
 
     const currentPath = usePage().url;
